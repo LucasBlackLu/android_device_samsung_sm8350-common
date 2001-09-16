e482803354eb738a58a7d8db5cbe6c734a5673e4
Tim Zimmermann <tim@linux4.de>
2001-09-17 00:00:00 +0200

    sm8350-common: squashed import of releasetools
    * Require T firmware
    
    Change-Id: I5ab3744ec2a61249d6d7dd5380b40f0ad3e7e7ba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,5 +123,9 @@ TARGET_USES_MKE2FS := true
 BOARD_USES_FULL_RECOVERY_IMAGE := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.default
 
+# Releasetools
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_samsung_sm8350
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
+
 # VNDK
 PRODUCT_VNDK_VERSION := current
