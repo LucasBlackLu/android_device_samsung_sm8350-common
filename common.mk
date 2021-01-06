a2a19773e5a53f9ad1614c9511394f008c6b969b
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    sm6375-common: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: I6d5eb8084127e295e4abec2c0192c0ad5b07b867

--- common.mk
+++ common.mk
@@ -265,6 +265,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
