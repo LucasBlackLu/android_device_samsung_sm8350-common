247a3596b5e76129a7239f48754bd8fc55a3f346
P.Adarsh Reddy <padarshr@codeaurora.org>
2021-05-04 15:29:23 +0530

    sm8350-common: Enforce android-S new launch debugfs restrictions
    Change-Id: I78878e1c4177c07d66b13e5659d4360252c5f87b

--- common.mk
+++ common.mk
@@ -127,6 +127,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V1-ndk_platform.vendor \
