b5ce301adadcf96ad1a478c1312036fa4e171220
Alexander Koskovich <zvnexus@outlook.com>
2021-07-26 02:17:02 -0700

    sm8350-common: Add LAHAINA overlays from LA.QSSI.11.0.r1-12400-qssi.0.
    Change-Id: I5152cbcf378a01f2482272b409eee6f49c0cdec5

--- common.mk
+++ common.mk
@@ -54,9 +54,13 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
+    FrameworksResTarget \
     SystemUIResCommon \
     TelephonyResCommon \
-    WifiResCommon
+    WifiResCommon \
+    WifiResCommon \
+    WifiResTarget
+
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
