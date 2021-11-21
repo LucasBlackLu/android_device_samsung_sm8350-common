a35b2d095699cf79dff1bdee52e8a912e0f4e7b9
Tim Zimmermann <tim@linux4.de>
2021-11-21 20:17:51 +0100

    sm8350-common: Uprev audio HAL to V7
    Updated audio policy by running:
    
    hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/samsung/sm7325-common/audio/configs/audio_policy_configuration.xml 6.0
    
    with some manual intervention.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514

--- common.mk
+++ common.mk
@@ -25,8 +25,8 @@ PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl.samsung-sm8350 \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl.samsung-sm8350 \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
     audio.usb.default \
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
