66b26ee48cab3ca312a0d54cedb80e46643f5b77
LuK1337 <priv.luk@gmail.com>
2022-02-21 17:34:52 +0100

    sm8350-common: Shim Samsung health HAL impl with libutils-v30.so
    Change-Id: Ia65ac92ebd215d8851cd50d8307180cdf47e3b6d

--- common.mk
+++ common.mk
@@ -309,6 +309,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
