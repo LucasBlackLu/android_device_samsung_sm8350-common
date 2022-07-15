e629fd867335d0bd5bf7cf4d9d0e58a4648be0be
Josip Kelecic <kelecicj@gmail.com>
2022-07-15 09:40:40 +0200

    sm8550-common: releasetools: flash vendor_boot
    Change-Id: I3ffe9ce5b4578453fe481ba05953fc4b3c27599b

--- releasetools.py
+++ releasetools.py
@@ -37,4 +37,5 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
+  AddImage(info, "vendor_boot.img", "/dev/block/by-name/vendor_boot")
   return
