8677bd105e0d25082b1c3e03a3220f2dfcccc7e5
chaptsand <chaptsand@gmail.com>
2023-05-05 19:18:52 +0800

    sm8350-common: Import kalama vendor overlays
    Change-Id: Ie9eabba9841ce8bded3998e99ce473a4bd60035b

--- common.mk
+++ common.mk
@@ -41,10 +41,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
+    FrameworksResTarget \
     SystemUIResCommon \
     TelecommResCommon \
     TelephonyResCommon \
-    WifiResCommon
+    WifiResCommon \
+    WifiResTarget
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
