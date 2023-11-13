96f870a364a67f1d13cf986c8872ea2efdfd7259
ReallySnow <reallysnow233@gmail.com>
2023-11-13 17:57:58 +0000

    sm8350-common: overlay: Define status bar padding & button location
    Change-Id: I132f8335e26500992d000c5e44097abaa0c35f1b

--- common.mk
+++ common.mk
@@ -56,6 +56,7 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget \
     LahainaFrameworks \
+    LahainaSystemUI \
     SystemUIResCommon \
     TelephonyResCommon \
     WifiResCommon \
