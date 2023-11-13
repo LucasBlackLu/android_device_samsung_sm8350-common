ceafc5120a68aaa8afe20e90b833bc8dbd28aeac
ReallySnow <reallysnow233@gmail.com>
2023-11-13 18:03:39 +0000

    sm8350-common: overlay: Enable smooth display
    Change-Id: I61020ac48aad8fc3c38f938139fd71cfb8f5b2fc

--- common.mk
+++ common.mk
@@ -45,6 +45,7 @@ PRODUCT_PACKAGES += \
     FrameworksResCommon \
     FrameworksResTarget \
     LahainaFrameworks \
+    LahainaSettings \
     LahainaSystemUI \
     SystemUIResCommon \
     TelephonyResCommon \
