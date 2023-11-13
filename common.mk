b3c3dbd9feff2b789234fe7eceffc985716d5b29
ReallySnow <reallysnow233@gmail.com>
2023-11-13 16:22:53 +0800

    sm8350-common: overlay: Configure status bar height and rounded corners
    Change-Id: I0d62310ec1ebc0950c03af462855e954955acb58

--- common.mk
+++ common.mk
@@ -44,6 +44,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
     FrameworksResTarget \
+    LahainaFrameworks \
     SystemUIResCommon \
     TelephonyResCommon \
     WifiResCommon \
