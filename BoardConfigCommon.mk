e5e45f4e6c15f7bf22385bd42332e92731886c3a
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    sm8350-common: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,6 +88,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
