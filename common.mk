017492a56fceebe063f8506b4d9ef95a45c33790
LucasBlackLu <lucasblack997@gmail.com>
2024-02-18 10:14:30 +0800

    sm8350-common:  Intial device configurations

---
+++ common.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# No A/B
+AB_OTA_UPDATER := false
+
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
