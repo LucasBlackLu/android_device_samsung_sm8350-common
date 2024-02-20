c64e862d21a400e6dc61be9eaf1f504c3854be4c
LucasBlackLu <lucasblack997@gmail.com>
2024-02-21 01:54:37 +0800

    sm8350-common: Update init script from G9960ZHU6FWK7

--- common.mk
+++ common.mk
@@ -24,7 +24,36 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
 
 PRODUCT_PACKAGES += \
-    fstab.default
+    fstab.default \
+    fstab.ramplus \
+    init.audio.samsung.rc \
+    init.fingerprint.rc \
+    init.nfc.samsung.rc \
+    init.qcom.rc \
+    init.qti.kernel.rc \
+    init.ramplus.rc \
+    init.recovery.qcom.rc \
+    init.recovery.samsung.rc \
+    init.samsung.bsp.rc \
+    init.samsung.display.rc \
+    init.samsung.rc \
+    init.target.rc \
+    init.vendor.rilcommon.rc \
+    init.vendor.sensors.rc \
+    ueventd.qcom.rc \
+    init.class_main.sh \
+    init.kernel.post_boot.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.kernel.sh \
+    init.qti.media.sh \
+    init.qti.qcv.sh \
+    vendor_modprobe.sh
 
 # No A/B
 AB_OTA_UPDATER := false
