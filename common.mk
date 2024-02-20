5a6fd97a60de04bbf580976ce13fc6acff2fe529
LucasBlackLu <lucasblack997@gmail.com>
2024-02-21 01:54:37 +0800

    sm8350-common: Update init script from G9960ZHU6FWK7

--- common.mk
+++ common.mk
@@ -26,7 +26,36 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
 
 PRODUCT_PACKAGES += \
-    fstab.default
+    fstab.default \
+    fstab.ramplus \
+    init.qcom.rc \
+    init.qti.kernel.rc \
+	init.qti.media.rc \
+    init.ramplus.rc \
+    init.samsung.bsp.rc \
+    init.samsung.display.rc \
+    init.samsung.connector.rc \
+    init.samsung.rc \
+    init.target.rc \
+	init.vendor.rilchip.rc \
+	init.vendor.rilcommon.rc \
+	init.vendor.sensors.rc \
+    ueventd.qcom.rc \
+	wifi_brcm.rc \
+    wifi_sec.rc \
+    init.class_main.sh \
+	init.kernel.post_boot.sh \
+	init.kernel.post_boot-lahaina.sh \
+	init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+	init.qti.chg_policy.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.kernel.sh \
+	init.qti.media.sh \
+    init.qti.qcv.sh \
+    vendor_modprobe.sh
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 30
