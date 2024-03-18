640d355ed1b0ee65ef1bec8acccfaf394a5a52ac
LuK1337 <priv.luk@gmail.com>
2024-03-18 18:18:12 +0100

    sm8350-common: Use libcrypto-v33 from hardware/lineage/compat
    Because vndk prebuilt has SONAME set to libcrypto.so, it prevents linker
    from loading libcrypto.so built from source.
    
    Change-Id: Ic9e29f7b00b469163f48ea407fc9b5d5e90816c8

--- common.mk
+++ common.mk
@@ -414,9 +414,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libcrypto-v33
+
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v33.so
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
 
 # WiFi
 PRODUCT_PACKAGES += \
