026142cb358c58d9726dc114d5f15ceed708d21a
ata-kaner <ata.kaner03@gmail.com>
2024-04-08 09:15:27 +0300

    sm8350-common: Inherit emulated_storage.mk
    With the deprecation of SdcardFS in kernel, we can now inherit emulated_storage without any problem.
    
    Signed-off-by: ata-kaner <ata.kaner03@gmail.com>

--- common.mk
+++ common.mk
@@ -5,6 +5,9 @@
 
 COMMON_PATH := device/samsung/sm8350-common
 
+# Enable Scoped Storage related
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Add common definitions for Qualcomm
 $(call soong_config_set,rfs,mpss_firmware_symlink_target,firmware_modem)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
