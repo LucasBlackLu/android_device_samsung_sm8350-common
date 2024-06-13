50eb8879af29bb84419e38a5380e5236bb496798
LucasBlackLu <lucasblack997@gmail.com>
2024-06-14 02:47:01 +0800

    sm8350-common: Import fstab.default from stock

--- common.mk
+++ common.mk
@@ -16,5 +16,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Rootdir
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default \
+    $(LOCAL_PATH)/rootdir/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
+
+PRODUCT_PACKAGES += \
+    fstab.default
+
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 30
