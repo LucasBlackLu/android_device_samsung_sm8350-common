f3fcfe4f5a14b134d663733bad9f429cf66f7f61
Lokesh Gidra <lokeshgidra@google.com>
2024-06-15 11:33:25 +0200

    sm8350-common: Use 6GB Dalvik-heap Config
    Bug: 145823510
    Test: MPTS
    Change-Id: I99b04d088330ad465d2fc09986e975c262f2bdad

--- common.mk
+++ common.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
