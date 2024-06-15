41b8ac6e00a3caf58f0cb97148aa16f957442844
SamarV-121 <samarvispute121@pm.me>
2024-06-15 17:39:42 +0200

    a71-common: vibrator: Add support for duration based amplitude control
    * As per https://review.lineageos.org/c/LineageOS/android_hardware_samsung/+/356121.
    
    Change-Id: Ic950e7ac19c61c9f186f701489f03dea6de451e2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -256,6 +256,9 @@ TARGET_USES_USB_GADGET_HAL := true
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
+# Vibrator
+$(call soong_config_set,samsungVibratorVars,duration_amplitude,true)
+
 # Enable chain partition for recovery.
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
