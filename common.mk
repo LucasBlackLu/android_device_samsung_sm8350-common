23754a59515d6a2aeddb51af71e5bdc91af0c349
LucasBlackLu <lucasblack997@gmail.com>
2024-06-18 22:24:17 +0800

    sm8350-common: Import QCOM scripts from LA.UM.9.14.r1-24300-LAHAINA.QSSI14.0

--- common.mk
+++ common.mk
@@ -7,6 +7,21 @@
 # No A/B
 AB_OTA_UPDATER := false
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default \
+    $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
+
+PRODUCT_PACKAGES += \
+    fstab.default \
+    init.class_main.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.rc \
+    init.qcom.sh \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
