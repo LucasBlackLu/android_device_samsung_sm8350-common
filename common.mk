47c93d5ae02d3144e0df1600a6b1478ae90cca59
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    sm8350-common: Add RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- common.mk
+++ common.mk
@@ -4,9 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Add common definitions for Qualcomm
+$(call soong_config_set,rfs,mpss_firmware_symlink_target,firmware_modem)
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
