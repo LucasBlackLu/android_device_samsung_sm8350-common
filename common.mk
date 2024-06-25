c3176c0c187589bc4dbdbf24fc187c411a8b303a
Reema Bajwa <reemabajwa@google.com>
2024-06-25 18:11:18 +0000

    gta9p-common: Enable Credential Manager for all devices
    Bug: 345373198
    Test: Cts
    
    Change-Id: I54ec933aaacef5c313dada751cd68d303a7cb0b7
    
    Author:    Reema Bajwa <reemabajwa@google.com>
    Date:      Tue Jun 25 18:11:18 2024 +0000

--- common.mk
+++ common.mk
@@ -200,6 +200,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Support Credential Manager
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.credentials.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.credentials.xml
+
 ## Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
