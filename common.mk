4a9fbd3c8532a167474e59a0b313f05eeb4b93c8
Niranjan BR <niranjankannan2003@gmail.com>
2024-07-14 10:19:22 +0530

    sm8350-common: Build missing VNDK libraries
    audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- common.mk
+++ common.mk
@@ -52,6 +52,8 @@ PRODUCT_PACKAGES += \
     libaudioroute \
     libaudioroute.vendor \
     libaudioutils \
+    libaudioroute.vendor\
+    libstagefright_softomx_plugin.vendor \
     libtinyalsa \
     libtinycompress \
     tinycap \
