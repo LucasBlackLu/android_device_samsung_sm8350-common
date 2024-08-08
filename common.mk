eb2425074f0b547228a9c9d79a65e8cbd75a90bb
L1ghtzin <L1ghtzin@users.noreply.github.com>
2024-08-08 08:50:22 +0000

    sm8350-common: Build AIDL Wifi HAL
    * Existent modules in PRODUCT_PACKAGES
    
    * Offending entries:
      android.hardware.wifi@1.0-service

--- common.mk
+++ common.mk
@@ -371,6 +371,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
+    android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwifi-hal \
     libwpa_client \
