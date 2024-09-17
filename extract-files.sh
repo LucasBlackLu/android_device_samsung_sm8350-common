e80e9b8b0f59c6b96e90241faa7ea50c101191ad
Michael Bestas <mkbestas@lineageos.org>
2024-09-17 15:43:15 +0800

    sm8350-common: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -14,10 +13,14 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
+
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
-	echo "Unable to find helper script at ${HELPER}"
-	exit 1
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
 fi
 source "${HELPER}"
 
@@ -25,61 +28,78 @@ source "${HELPER}"
 CLEAN_VENDOR=true
 
 ONLY_COMMON=
+ONLY_FIRMWARE=
 ONLY_TARGET=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
-	case "${1}" in
-	--only-common)
-		ONLY_COMMON=true
-		;;
-	--only-target)
-		ONLY_TARGET=true
-		;;
-	-n | --no-cleanup)
-		CLEAN_VENDOR=false
-		;;
-	-k | --kang)
-		KANG="--kang"
-		;;
-	-s | --section)
-		SECTION="${2}"
-		shift
-		CLEAN_VENDOR=false
-		;;
-	*)
-		SRC="${1}"
-		;;
-	esac
-	shift
+    case "${1}" in
+    --only-common)
+        ONLY_COMMON=true
+        ;;
+    --only-firmware)
+        ONLY_FIRMWARE=true
+        ;;
+    --only-target)
+        ONLY_TARGET=true
+        ;;
+    -n | --no-cleanup)
+        CLEAN_VENDOR=false
+        ;;
+    -k | --kang)
+        KANG="--kang"
+        ;;
+    -s | --section)
+        SECTION="${2}"
+        shift
+        CLEAN_VENDOR=false
+        ;;
+    *)
+        SRC="${1}"
+        ;;
+    esac
+    shift
 done
 
 if [ -z "${SRC}" ]; then
-	SRC="adb"
+    SRC="adb"
 fi
 
 function blob_fixup() {
-	case "${1}" in
-	vendor/lib64/hw/gatekeeper.mdfpp.so | vendor/lib64/libskeymaster4device.so)
-		"${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
-		;;
-	esac
+    case "${1}" in
+    vendor/lib64/hw/gatekeeper.mdfpp.so | vendor/lib64/libskeymaster4device.so)
+        [ "$2" = "" ] && return 0
+        "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+        ;;
+    esac
+
+    return 0
 }
 
-if [ -z "${ONLY_TARGET}" ]; then
-	# Initialize the helper for common device
-	setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
 
-	extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+if [ -z "${ONLY_FIRMWARE}" ] && [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
+
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
-	# Reinitialize the helper for device
-	source "${MY_DIR}/../../${VENDOR}/${DEVICE}/extract-files.sh"
-	setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+    # Reinitialize the helper for device
+    source "${MY_DIR}/../../${VENDOR}/${DEVICE}/extract-files.sh"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-	extract "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    if [ -z "${ONLY_FIRMWARE}" ]; then
+        extract "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    fi
+
+    if [ -z "${SECTION}" ] && [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
+        extract_firmware "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" "${SRC}"
+    fi
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
