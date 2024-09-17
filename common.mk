e4623c47b523f78481eebf58425858693b3750d0
LucasBlackLu <lucasblack997@gmail.com>
2024-09-17 22:17:07 +0800

    sm8350-common: Import fstab.default from stock

--- common.mk
+++ common.mk
@@ -4,6 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+COMMON_PATH := device/samsung/sm8350-common
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default \
+    $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
+
+PRODUCT_PACKAGES += \
+    fstab.default
+
 # No A/B
 AB_OTA_UPDATER := false
 
