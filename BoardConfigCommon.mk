93916149ca5da541ef50cd832b12aa3ed4609c07
LucasBlackLu <lucasblack997@gmail.com>
2024-09-20 21:49:50 +0800

    sm8350-common: Set TARGET_SEPOLICY_DIR to lahaina

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,6 +170,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
+TARGET_SEPOLICY_DIR := lahaina
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # Security
