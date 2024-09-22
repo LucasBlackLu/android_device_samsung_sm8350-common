a1006cd07e6fba50d2b6329bbe5e21a8d30f35d9
LucasBlackLu <lucasblack997@gmail.com>
2024-09-22 09:42:44 +0800

    sm8350-common: Upldate kernel flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,44 +41,28 @@ TARGET_BOOTLOADER_BOARD_NAME := lahaina
 TARGET_NO_BOOTLOADER := true
 
 # Kernel config
-TARGET_KERNEL_SOURCE        := kernel/samsung/sm8350
-TARGET_KERNEL_ARCH          := arm64
-TARGET_KERNEL_HEADER_ARCH   := arm64
-TARGET_LINUX_KERNEL_VERSION := 5.4
-
-# Kernel flags
-BOARD_KERNEL_CMDLINE += console=null androidboot.hardware=qcom 
-BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1 
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 
-BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1 
-BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 
-BOARD_KERNEL_CMDLINE += loop.max_part=7 cgroup.memory=nokmem,nosocket 
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image 
-BOARD_KERNEL_CMDLINE += printk.devkmsg=on pcie_ports=compat 
-BOARD_KERNEL_CMDLINE += cpuinfo.chipname=SM8350 panic=4
+BOARD_KERNEL_BINARIES := kernel
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 swiotlb=0
+BOARD_KERNEL_CMDLINE += loop.max_part=7 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += pcie_ports=compat loop.max_part=7
+BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_BOOT_HEADER_VERSION := 3
-
-BOARD_KERNEL_BASE            := 0x00000000
-BOARD_KERNEL_PAGESIZE        := 4096
-BOARD_RAMDISK_OFFSET         := 0x02000000
-BOARD_DTB_OFFSET             := 0x01f00000
-BOARD_KERNEL_OFFSET          := 0x00008000
-BOARD_KERNEL_TAGS_OFFSET     := 0x01e00000
-BOARD_KERNEL_IMAGE_NAME      := Image
-BOARD_KERNEL_SEPARATED_DTBO  := true
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS := --header_version 3
+KERNEL_LD := LD=ld.lld
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_APPEND_DTB := false
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_RAMDISK_USE_LZ4        := true
-
-# Kernel: mkbootimgs args
-BOARD_CUSTOM_BOOTIMG := true
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --board $(BOARD_NAME)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/samsung/sm8350
 
 # Additional root folders
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/configs/config.fs
