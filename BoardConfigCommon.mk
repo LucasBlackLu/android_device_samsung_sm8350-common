dbec0874a9810ab7ca901aff4eeb6ba744fdb6a9
LucasBlackLu <lucasblack997@gmail.com>
2024-10-08 20:23:37 +0800

    Revert "sm8350-common: Add libinit"
    This reverts commit 68030da2131ef6ca19f3eacf1338d2c43171a81c.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,10 +99,6 @@ TARGET_KEYMASTER_VARIANT := samsung
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sm8350
-TARGET_RECOVERY_DEVICE_MODULES := libinit_sm8350
-
 # Partitions, obtained with blockdev --getsize64
 BOARD_BOOTIMAGE_PARTITION_SIZE        := 100663296 #blockdev --getsize64 /dev/block/bootdevice/by-name/boot
 BOARD_CACHEIMAGE_PARTITION_SIZE       := 629145600 #blockdev --getsize64 /dev/block/bootdevice/by-name/boot
