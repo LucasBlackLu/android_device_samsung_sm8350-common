90186d25c847c70785ce147679e683f8198675f3
Michael Bestas <mkbestas@gmail.com>
2024-10-11 14:12:53 -0300

    sm8350-common: Switch to AIDL USB HALs
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- common.mk
+++ common.mk
@@ -381,7 +381,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
