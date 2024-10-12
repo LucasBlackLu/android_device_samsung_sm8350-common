a50c13529e8679f326459965c6103b1d34e2c85e
LucasBlackLu <lucasblack997@gmail.com>
2024-10-12 13:48:15 +0800

    sm8350-common: Import init files from G9960ZHU7GXDE

--- common.mk
+++ common.mk
@@ -26,7 +26,39 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
 
 PRODUCT_PACKAGES += \
-    fstab.default
+    fstab.default \
+    fstab.ramplus \
+    init.audio.samsung.rc \
+    init.fingerprint.rc \
+    init.nfc.samsung.rc \
+    init.qcom.rc \
+    init.qti.kernel.rc \
+    init.ramplus.rc \
+    init.samsung.bsp.rc \
+    init.samsung.connector.rc \
+    init.samsung.display.rc \
+    init.samsung.rc \
+    init.target.rc \
+    init.vendor.rilcommon.rc \
+    init.vendor.sensors.rc \
+    ueventd.qcom.rc \
+    vendor.samsung.rilchip.qcom.rc \
+    wifi_brcm.rc \
+    wifi_sec.rc
+
+# Vendor scripts
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.kernel.post_boot.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.qti.kernel.sh \
+    init.qti.chg_policy.sh \
+    init.qti.qcv.sh \
+    vendor_modprobe.sh
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
