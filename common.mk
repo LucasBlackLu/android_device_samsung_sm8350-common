e78d90d6207928d8ccc4bd2e2c5b7fc0fa8b9e0a
LucasBlackLu <lucasblack997@gmail.com>
2024-10-12 13:48:15 +0800

    sm8350-common: Import init files from G9960ZHU7GXDE

--- common.mk
+++ common.mk
@@ -10,7 +10,37 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     fstab.default \
-    fstab.default.vendor_ramdisk
+    fstab.default.vendor_ramdisk \
+    fstab.ramplus \
+    init.audio.samsung.rc \
+    init.fingerprint.rc \
+    init.nfc.samsung.rc \
+    init.qcom.rc \
+    init.qti.kernel.rc \
+    init.ramplus.rc \
+    init.samsung.bsp.rc \
+    init.samsung.connector.rc \
+    init.samsung.display.rc \
+    init.samsung.rc \
+    init.target.rc \
+    init.vendor.rilcommon.rc \
+    init.vendor.sensors.rc \
+    ueventd.rc \
+    vendor.samsung.rilchip.qcom.rc
+
+# Vendor scripts
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.kernel.post_boot.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    init.qti.kernel.sh \
+    init.qti.chg_policy.sh \
+    init.qti.qcv.sh \
+    vendor_modprobe.sh
 
 # No A/B
 AB_OTA_UPDATER := false
