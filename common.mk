e0c5478560423506f993f7c8875373a2b11d3e33
LucasBlackLu <lucasblack997@gmail.com>
2024-10-12 22:07:47 +0800

    sm8350-common: Import ipa_fws.rc and bind

--- common.mk
+++ common.mk
@@ -127,6 +127,7 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     init.vendor.rilcommon.rc \
     init.vendor.sensors.rc \
+    ipa_fws.rc \
     ueventd.qcom.rc \
     vendor.samsung.rilchip.qcom.rc
 
