7db73fbfe5599cd36b087bc72d7b113eea38795f
LucasBlackLu <lucasblack997@gmail.com>
2024-10-14 23:17:46 +0800

    sm8350-common: Include hardware/samsung FCM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,7 @@ TARGET_NO_RPC := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
