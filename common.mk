LineageOS/android_device_oneplus_sm8350-common

sm8350-common: Make fstab available to vendor ramdisk
Change-Id: I7a34ac9fb0e0779669bdbb8aa2a91302a0fac101

common.mk
@@ -276,11 +276,9 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml
 
 # Init
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
-
 PRODUCT_PACKAGES += \
     fstab.default \
+    fstab.default.vendor_ramdisk \
     init.class_main.sh \
     init.oplus.display.rc \
     init.oplus.radio.rc \

LucasBlackLu/android_device_samsung_sm8350-common

common.mk
@@ -6,11 +6,11 @@
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
 PRODUCT_PACKAGES += \
     fstab.qcom\
+    fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \