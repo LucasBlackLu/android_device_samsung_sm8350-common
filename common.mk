13cfaa27b66e9d869459afbb826879c7509f3b44
Arvind Kumar <quic_c_karvin@quicinc.com>
2024-11-06 09:22:20 +0100

    gta9p-common: Add "android.software.credentials.prebuilt.xml" to system image
    Include android.software.credentials.prebuilt.xml
    prebuilt_etc module to add feature 'android.software.credentials'
    
    Bug: 344511668
    
    Test: See if android.software.credentials.xml is installed in
          system/etc/permissions
    
    Co-authored-by: Justin Yun <justinyun@google.com>
    Change-Id: Iaa265f533520e7f4851d3c088ddc54ef8c649013
    (cherry picked from commit 56d4c9d7ca7da10f1cb3c38e866993ceff4ee865)

--- common.mk
+++ common.mk
@@ -200,9 +200,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Support Credential Manager
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.credentials.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.credentials.xml
+## Support Credential Manager
+PRODUCT_PACKAGES += \
+    android.software.credentials.prebuilt.xml
 
 ## Power
 PRODUCT_PACKAGES += \
