f5dd3ec8dedc70d845250fb374e9d85e1557a772
Alexander Koskovich <zvnexus@outlook.com>
2025-01-04 21:29:27 +0100

    sm8350-common: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,7 +33,6 @@ TARGET_LINUX_KERNEL_VERSION := 5.4
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_CMDLINE += console=null androidboot.hardware=qcom \
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1 \ 
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 \
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1 \
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 \
 BOARD_KERNEL_CMDLINE += swiotlb=2048 loop.max_part=7 \
