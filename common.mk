546db49a5728644612c9de0f81a0089b62feeb4b
LuK1337 <priv.luk@gmail.com>
2025-01-17 10:27:55 +0100

    sm6375-common: Build QTI USB HAL
    Change-Id: Id502964e8508534c84cc03cae3662ff2728d98a7

--- common.mk
+++ common.mk
@@ -258,6 +258,9 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.qti
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
