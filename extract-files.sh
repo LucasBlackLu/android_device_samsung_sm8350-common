4a2690d82576c7f3d18a4ad6fc6cf93bfef58857
LucasBlackLu <lucasblack997@gmail.com>
2025-01-19 20:04:33 +0800

    sm8350-common: Add to ELF check
    out/soong/.intermediates/vendor/samsung/sm8350-common/libkeymaster_helper/android_vendor_arm64_armv8-a_cortex-a76_shared/e560d7b19ebf7276b3e850d3d346dec8/libkeymaster_helper.so:
    note: If this is a new prebuilt file and it is designed to have
    unresolved symbols, add one of the following properties:
    out/soong/.intermediates/vendor/samsung/sm8350-common/libkeymaster_helper/android_vendor_arm64_armv8-a_cortex-a76_shared/e560d7b19ebf7276b3e850d3d346dec8/libkeymaster_helper.so:
    note:   Android.bp: allow_undefined_symbols: true,
    out/soong/.intermediates/vendor/samsung/sm8350-common/libkeymaster_helper/android_vendor_arm64_armv8-a_cortex-a76_shared/e560d7b19ebf7276b3e850d3d346dec8/libkeymaster_helper.so:
    note:   Android.mk: LOCAL_ALLOW_UNDEFINED_SYMBOLS := true

--- extract-files.sh
+++ extract-files.sh
@@ -61,7 +61,7 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-	vendor/lib64/hw/gatekeeper.mdfpp.so | vendor/lib64/libskeymaster4device.so)
+	vendor/lib64/hw/gatekeeper.mdfpp.so | vendor/lib64/libskeymaster4device.so | vendor/lib64/libkeymaster_helper.so)
 		"${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
 		;;
     esac
