8b8fb01becfa576b9841abad2e8456ffb26036ae
Haky86 <hakamelassouad@gmail.com>
2025-02-03 00:30:58 +0100

    sm8350-common: Initial commit
    Change-Id: Id897e456b7fa06fda103bca961623c65858f4e17

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/sm8350-common
+
+# Get non-open-source specific aspects
+include vendor/samsung/sm8350-common/BoardConfigVendor.mk
---
+++ common.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/sm8350-common
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
