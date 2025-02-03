228c900299ca4bd976c25fcb56cebad7624c19b3
Lajos Molnar <lajos@google.com>
2025-02-03 01:08:18 +0100

    sm6375-common: Initial media_codecs_google*.xml files
    Separated description of Google-provided codecs into their own
    files based on domain.  Google-provided audio/telephony codecs
    should be included prior to specifying vendor codecs.  Google-
    provided video codecs should be included after vendor codecs.
    
    Bug: 13549114
    Change-Id: I216f01feaf0379c6cd7d8714e02792fdf2214a90

--- common.mk
+++ common.mk
@@ -171,6 +171,11 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(COMMON_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
+
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
