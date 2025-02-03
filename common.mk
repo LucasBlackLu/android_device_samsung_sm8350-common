e9f597363edae0cee4951c37aff061a449372995
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-03 01:13:37 +0100

    sm8350-common: Import sensors configs
    * Taken from a23xqnsxx-user 14 UP1A.231005.007 A236BXXS9EYA1 release-keys.
    
    Change-Id: Ifdc949ff5c8e008de4d58162fd90f9cd91d3581d

--- common.mk
+++ common.mk
@@ -291,5 +291,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/seccomp_policy,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(COMMON_PATH)/configs/sensors/sns_reg_config:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sns_reg_config \
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
