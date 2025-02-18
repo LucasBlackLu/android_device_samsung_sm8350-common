037fc5402fe4e15b5d45873f443b6da2a8781bb4
Quallenauge <Hamsi2k@freenet.de>
2025-02-18 10:23:27 -0300

    sm8350-common: Get rid of firmware_class.path.
    Kernel tries to load the firmware from the internal paths and from additional
    paths set with firmware_class.path. This leads to EACCES (-13	/* Permission denied */)
    errors within the log.
    After that the ueventd fallback is used to load the firmware,
    which loads without any noises, since the addional path ( /vendor/firmware_mnt/image/ )
    is set as firmware_directories parameter.
    Even tough the firmware is loadíng at the end, removing the kernel parameter silents
    the first error messages from the first attempt which might confuse
    a hobby kernel log reader.
    
    * Fix following errors
    ueventd: firmware: could not find firmware for ipa_fws.mdt
    ueventd: firmware: attempted /etc/firmware/ipa_fws.mdt, open failed: No such file or directory
    ueventd: firmware: attempted /odm/firmware/ipa_fws.mdt, open failed: No such file or directory
    ueventd: firmware: attempted /vendor/firmware/ipa_fws.mdt, open failed: No such file or directory
    ueventd: firmware: attempted /firmware/image/ipa_fws.mdt, open failed: No such file or directory

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,7 +134,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_sm8350
 # Kernel
 BOARD_DTB_OFFSET := 0x01f00000
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware_mnt/image printk.devkmsg=on pcie_ports=compat cpuinfo.chipname=SM8350 panic=4 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket printk.devkmsg=on pcie_ports=compat cpuinfo.chipname=SM8350 panic=4 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_OFFSET := 0x00008000
