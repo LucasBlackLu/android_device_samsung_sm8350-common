bb8fe89b9da1c9fb67c86cffd592a6fc5e7ad468
LuK1337 <priv.luk@gmail.com>
2025-02-20 00:14:36 +0100

    sm8350-common: Shim Samsung health HAL impl with libutils-v30.so
    Change-Id: Ia65ac92ebd215d8851cd50d8307180cdf47e3b6d

--- common.mk
+++ common.mk
@@ -333,6 +333,10 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so \
+
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
     vndservicemanager \
