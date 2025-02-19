3929dd7ace71a70f7317649ba04942586d7e168a
Colin Cross <ccross@android.com>
2025-02-19 10:59:43 +0100

    sm8350-common: Add core_64_bit.mk
    Add a new product, core_64_bit.mk, that products can inherit from
    to configure zygote and the rest of the build system for a standard
    64-bit product.
    
    Make the 64-bit emulator targets for arm64, mips64, and x86_64
    inherit from it.
    
    Change-Id: I7e809264db39472f554cd5290529f3d6499345d4

--- common.mk
+++ common.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 COMMON_PATH := device/samsung/sm8350-common
 
 # Get non-open-source specific aspects
