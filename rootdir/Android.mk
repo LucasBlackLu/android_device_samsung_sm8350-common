629745c02b434ca6c7d1a3a3295557fb612e98c1
Haky86 <hakamelassouad@gmail.com>
2025-02-19 11:09:43 +0100

    sm8350-common: Setup default recovery flags
    * Build and boot a working LineageOS recovery.
    
    Change-Id: Icc5e7fddce0765a73afc0ce4c108e102824a28fa

---
+++ rootdir/Android.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+LOCAL_MODULE_TAGS  := optional
+LOCAL_SRC_FILES    := vendor/etc/fstab.qcom
+include $(BUILD_PREBUILT)
