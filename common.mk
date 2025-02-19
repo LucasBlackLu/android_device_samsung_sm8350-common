b3460c14f0d4d46b0841709397593979f4716d04
Haky86 <hakamelassouad@gmail.com>
2025-02-19 14:22:46 +0100

    sm6375-common: Build NFC NXP libs from source
    Change-Id: I19d97d137eab729016dc93a382c7e39a057d72b4

--- common.mk
+++ common.mk
@@ -314,6 +314,9 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0:64 \
+    android.hardware.nfc@1.1:64 \
+    android.hardware.nfc@1.2:64 \
     android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     libchrome \
@@ -323,6 +326,8 @@ PRODUCT_PACKAGES += \
     NfcNci \
     SecureElement \
     Tag \
+    vendor.nxp.nxpese@1.0:64 \
+    vendor.nxp.nxpnfc@1.0:64 \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
