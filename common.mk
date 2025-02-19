533b5bacd5b04518884d7206e3911852584b14db
shoudil <stephenli@codeaurora.org>
2025-02-19 14:32:24 +0100

    sm6375-common: Declare that we support full value addons
    Change-Id: I0b8f2c5a2957b52b7d1c240b92059297b464b7de

--- common.mk
+++ common.mk
@@ -410,6 +410,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
 
 # QMI
+TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/qmi,$(TARGET_COPY_OUT_VENDOR)/etc/) \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
