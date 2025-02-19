ed36982c0aaf7b46834a3bd327f9e0063ab70ad5
LuK1337 <priv.luk@gmail.com>
2025-02-20 00:14:36 +0100

    sm8350-common: Shim Samsung health HAL impl with libutils-v30.so
    Change-Id: Ia65ac92ebd215d8851cd50d8307180cdf47e3b6d

--- common.mk
+++ common.mk
@@ -476,6 +476,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so \
+
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
     vndservicemanager \
--- extract-files.sh
+++ extract-files.sh
@@ -22,6 +22,10 @@ source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
+            # Replace libutils with vndk30 libutils
+            "${PATCHELF}" --replace-needed libutils.so libutils-v30.so "${2}"
+            ;;
         vendor/lib64/libsec-ril.so)
             sed -i 's/ril.dds.call.ongoing/vendor.calls.ongoing/g' "${2}"
             ;;
