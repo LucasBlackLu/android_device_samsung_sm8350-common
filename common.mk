d6b5fd07824413acb59a91bc5502379f651bc262
Jean-Baptiste Queru <jbq@google.com>
2025-02-19 11:00:01 +0100

    sm6375-common: Improve setup of base products
    Split telephony into its own product, so that we can easily
    add telephony support to any configuration without pulling
    anything else. Use it from generic.
    
    Split the "full" family between a (no-telephony) base,
    a telephony version of the same (both of which can be used
    as a starting point for on-device builds), and the true classic
    full which is an emulator build.
    
    Change-Id: I616d34f2a29415b8ec997173f8e893c312ade5c2

--- common.mk
+++ common.mk
@@ -6,6 +6,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 COMMON_PATH := device/samsung/sm8350-common
 
