5cfaf79a242c93a6ab9d8baa8e9b815add8ff0db
Alexander Koskovich <akoskovich@pm.me>
2025-02-19 14:02:43 +0100

    sm6375-common: Build QTI camera interfaces for CamX
    Change-Id: I23efb141c135c279f4bfb5c6f0b6c95b6c78a91d

--- common.mk
+++ common.mk
@@ -130,6 +130,8 @@ PRODUCT_PACKAGES += \
     libgrallocusage.vendor \
     libstdc++ \
     libstdc++_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
 
 # Charger
 PRODUCT_PACKAGES += \
