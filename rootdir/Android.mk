5bc452f132be290fb7383afe9e5026fa2a1666fe
Alistair Strachan <astrachan@google.com>
2025-02-19 11:11:11 +0100

    sm6375-common: rootdir: Use first_stage_mount feature
    This allows us to deprecate the dt fstab dependency.
    
    Change-Id: I9c2a8c140ed64faecee0d66e00033fdffa398a4b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -13,3 +13,12 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := vendor/etc/fstab.qcom
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom_ramdisk
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+LOCAL_MODULE_STEM  := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_SRC_FILES    := vendor/etc/fstab.qcom
+include $(BUILD_PREBUILT)
