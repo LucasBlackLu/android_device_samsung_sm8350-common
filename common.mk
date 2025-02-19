fe4635bbe2a8e807c49d8099b8d74fde4cf15e67
Wei Wang <wvw@google.com>
2025-02-19 13:53:35 +0100

    sm6375-common: Add atrace HAL 1.0
    Bug: 111098596
    Test: vts, atrace and systrace
    
    (cherry picked from commit db895ad8da1008082521146eac3647907a56a2b1)
    
    Change-Id: Iea4ee4bff7509031a6daaf7c9570d0583a59affa

--- common.mk
+++ common.mk
@@ -27,6 +27,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service \
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.primary.default \
