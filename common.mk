cbc52a1135e0caad14295540c6d307970c065630
Stricted <info@stricted.net>
2025-02-19 14:07:20 +0100

    sm6375-common: Build `AdvancedDisplay` from source
    * Built from hardware/samsung.
    
    Change-Id: I2ebaa180c54b7391f64cfed1a70470323f126c2c

--- common.mk
+++ common.mk
@@ -354,6 +354,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2 \
     libxml2 \
 
+# SamsungAdvancedDisplay
+PRODUCT_PACKAGES += \
+    AdvancedDisplay \
+
 # Seccomp Policy
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/seccomp_policy,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
