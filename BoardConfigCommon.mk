1e310759b06a2874f1174e9151ae25ee010539a0
Pooja Singh <spooja@codeaurora.org>
2025-02-19 11:46:15 +0100

    sm6375-common: Enable chain partition for recovery
    Change-Id: I81266c7ce170f9a108a0ce4d01980b5bf8faffd0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,6 +130,12 @@ BOARD_VNDK_VERSION := current
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
+# Enable chain partition for recovery.
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
 # Enable chain partition for system.
 BOARD_AVB_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_SYSTEM_ALGORITHM := SHA256_RSA2048
