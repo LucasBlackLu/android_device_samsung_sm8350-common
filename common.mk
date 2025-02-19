7bda4e08ac11af3298436245e0d86bc90887577a
Haky86 <hakamelassouad@gmail.com>
2025-02-19 11:11:45 +0100

    sm8350-common: Setup default RIL flags
    Change-Id: Ie9eecc8794d554ded7bcb2f34b4e9b8697502aba

--- common.mk
+++ common.mk
@@ -281,5 +281,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
 
+# RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.2 \
+    libxml2 \
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
