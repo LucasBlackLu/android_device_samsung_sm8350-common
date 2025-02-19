ddc02d2852c1de237b80785a18159f5e17628fa5
Stan Rokita <srok@google.com>
2025-02-19 14:33:53 +0100

    sm6375-common: Build the v2.1 sensor mutli HAL service
     * The multi HAL framework is a HAL interface for the
       sensors framework that allows multiple vendors to
       package their HAL implementation into sub HAL dynamic
       libraries that will be loaded and used to pass on
       method calls to the appropriate subHAL.
    
     * The HalProxy object, that will act as the main proxy
       sensors wrapper for the multiHAL handles writing sensor
       events to the event FMQ and wakelock acquisition and
       releasing via a callback object it passes to the subHALs.
    
    Change-Id: I51348b717f8b08491ac4fcfc33317b5fe92c55aa

--- common.mk
+++ common.mk
@@ -509,6 +509,9 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/seccomp_policy,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal \
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(COMMON_PATH)/configs/sensors/sns_reg_config:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sns_reg_config \
