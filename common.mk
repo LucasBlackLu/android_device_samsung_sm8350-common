1b1ef0345be775de9d5de5597aa122e7639dcf13
dianlujitao <dianlujitao@lineageos.org>
2025-02-19 13:38:10 +0100

    sm6375-common: overlay: Drop Snap
    This reverts commit b1891303f5c99b74631f5e06937fd5b050f53665.
    
    Change-Id: Ie5e2c3d337a3d80795d6d307eb5cc4326d73bbd0

--- common.mk
+++ common.mk
@@ -89,7 +89,6 @@ PRODUCT_PACKAGES += \
     camera.device@3.3-impl \
     camera.device@3.4-impl \
     camera.device@3.5-impl \
-    Snap \
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -193,7 +192,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(COMMON_PATH)/overlay-lineage/lineage-sdk \
-    $(COMMON_PATH)/overlay-lineage/packages/apps/Snap \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
