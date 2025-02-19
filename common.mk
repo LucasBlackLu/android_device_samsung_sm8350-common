df0c6c1dfbabd9b96894b293946b0156d5b14f81
Haky86 <hakamelassouad@gmail.com>
2025-02-19 14:18:28 +0100

    sm8350-common: Build lights stack from source
    Change-Id: Ie77c187bbb56ba675bde7561c6e4c1a01f9e60a8

--- common.mk
+++ common.mk
@@ -265,6 +265,12 @@ PRODUCT_PACKAGES += \
     libkeymaster4support.vendor:64 \
     libkeymaster4_1support.vendor:64 \
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    lights.qcom \
+
 # Media
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
