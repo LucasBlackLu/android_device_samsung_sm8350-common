68c8cfbda9c1e278dfb546cd7cf905da7cbd4ae3
Atakan <ata.kaner03@gmail.com>
2025-02-20 00:23:08 +0100

    sm8350-common: Switch to QTI health HAL
    Change-Id: I4444df3f7f6c9cb012674a55b44c31aa8f9a65d3

--- extract-files.sh
+++ extract-files.sh
@@ -22,10 +22,6 @@ source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
-            # Replace libutils with vndk30 libutils
-            "${PATCHELF}" --replace-needed libutils.so libutils-v30.so "${2}"
-            ;;
         vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
