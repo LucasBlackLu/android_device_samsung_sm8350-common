0226739ff6065c3c69171b7a347d71c6d48823b4
Jan Altensen <info@stricted.net>
2025-02-21 00:17:07 +0100

    sm6375-common: Exclude audiofx
    Change-Id: Ic04fdeeae61056edfa9d17829ba9638195722b98

--- common.mk
+++ common.mk
@@ -96,6 +96,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Automotive/Gearhead
 PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@2.0 \
