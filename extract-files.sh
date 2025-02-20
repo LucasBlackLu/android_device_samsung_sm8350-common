pjgowtham/android_device_oneplus_sm8350-common

sm8350-common: Shim libwvhidl with libcrypto_shim
The following change
https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
removed symbols that our libs require.

Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

extract-files.sh
@@ -127,6 +127,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
+        odm/lib64/libwvhidl.so|odm/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         *)
             return 1
             ;;

LucasBlackLu/android_device_samsung_sm8350-common

extract-files.sh
@@ -64,6 +64,9 @@ function blob_fixup() {
 	vendor/lib64/hw/gatekeeper.mdfpp.so | vendor/lib64/libskeymaster4device.so | vendor/lib64/libkeymaster_helper.so)
 		"${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
 		;;
+    vendor/lib/libwvhidl.so|vendor/lib/mediadrm/libwvdrmengine.so)
+        "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+        ;;
     esac
 }
 