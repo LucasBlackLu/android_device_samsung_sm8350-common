d96fcb097512e5812d3d6f0f416f4060b69f3e3d
Rashed Abdel-Tawab <rashed@linux.com>
2025-02-20 13:06:49 +0100

    sm8350-common: Import audio@7.0 default impl
    Change-Id: I37dac8d63dff454a396e9c14a1062db7f4b2588d

--- common.mk
+++ common.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@6.0-impl.samsung-sm8350 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
