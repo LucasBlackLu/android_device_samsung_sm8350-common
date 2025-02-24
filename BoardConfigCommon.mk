6c8019cd4a4e0702149ac244192c430b391bec92
LucasBlackLu <lucasblack997@gmail.com>
2025-02-24 08:41:03 +0800

    sm8350-common: init: Adapt for samsung sm8350
    *  Kang from `https://github.com/Simon1511/android_device_samsung_sm7325-common/tree/lineage-21/init`

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,8 +112,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/manifest_lahaina.xml
 
-DEVICE_MATRIX_FILE := \
-    $(COMMON_PATH)/compatibility_matrix.xml
+# Init
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sm8350
+TARGET_RECOVERY_DEVICE_MODULES := libinit_sm8350
 
 # Get non-open-source specific aspects
 include vendor/samsung/sm8350-common/BoardConfigVendor.mk
