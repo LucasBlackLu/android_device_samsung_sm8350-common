092987e865c7235dfc10361362ff2c4fbcf173d8
Dan Morrill <morrildl@google.com>
2025-03-03 11:09:28 +0100

    sm6375-common: Declare support for the Bluetooth API
    Change-Id: I1ab85b3085af8c2ff3bbf6db368439075101849f

--- common.mk
+++ common.mk
@@ -123,6 +123,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-legacy \
