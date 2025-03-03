471bb588ffecf9cbc8cfb4e9f8580e856b5ae1ef
Vinit Deshapnde <vinitd@google.com>
2025-03-03 12:13:25 +0100

    sm6375-common: Build the QTI WiFI HAL implementation
    Change-Id: I104d9b9ee19588953b64d190d50dc4fe55f2fd84

--- common.mk
+++ common.mk
@@ -586,8 +586,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
+    libwifi-hal \
+    libwifi-hal-ctrl \
+    libwifi-hal-qcom \
+    libwpa_client \
     TetheringConfigOverlay \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
     WifiOverlay \
+    wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf \
 
