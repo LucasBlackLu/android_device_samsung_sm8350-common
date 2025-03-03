b17b1bee50a7b1588d41bca067e5cb2e0955aceb
Fenglin Wu <fenglinw@codeaurora.org>
2025-03-03 12:00:30 +0100

    sm8350-common: Build AIDL QTI vibrator impl from source
    Change-Id: I4a2ffdb36d4f24fce923b1023037e73451bdcc7e

--- common.mk
+++ common.mk
@@ -571,6 +571,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vndservicemanager \
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.impl \
+    vendor.qti.hardware.vibrator.service \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
