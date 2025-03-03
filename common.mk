28a1b3b371d189c6beb8178028ebfabec45727ed
Dan Morrill <morrildl@google.com>
2025-03-03 11:49:58 +0100

    sm6375-common: Declare standard set of features for a GSM phone
    Change-Id: I5b10c52f9744b437f07ce2e4ee4f841c4fd22540

--- common.mk
+++ common.mk
@@ -547,6 +547,7 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
 
 # WiFi
 PRODUCT_PACKAGES += \
