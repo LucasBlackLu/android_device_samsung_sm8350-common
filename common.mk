221d6abff75fa673b4407f869518887e8e9b9f98
Kyle Zhang <kelzhan@google.com>
2025-03-03 11:17:52 +0100

    sm6375-common: Use aidl drm interface
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I01ceb9bdb60ff6d9323acee3d7140b68b31a465f

--- common.mk
+++ common.mk
@@ -173,7 +173,7 @@ $(call inherit-product, vendor/qcom/opensource/dataservices/dataservices_vendor_
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor \
 
 # Fasatboot
