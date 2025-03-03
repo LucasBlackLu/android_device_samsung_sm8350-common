bede194c18a19adf62ba6dba4b0b2714fbe1eade
Henry Fang <quxiangfang@google.com>
2025-03-03 11:13:49 +0100

    sm6375-common: Update cas@1.1 hal to cas@1.2
    Test: Manual
    bug: 141783130
    Change-Id: I0c3e9041a2057ce721a608cf3a9f0a9bb15a2305
    Merged-In: I0c3e9041a2057ce721a608cf3a9f0a9bb15a2305

--- common.mk
+++ common.mk
@@ -155,6 +155,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
 
+# CAS
+PRODUCT_PACKAGES += \
+    android.hardware.cas@1.2-service-lazy \
+
 # Charger
 PRODUCT_PACKAGES += \
     libsuspend \
