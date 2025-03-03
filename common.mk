bc7b40c804594fd0a4efcf49a698dbf9e808523f
Jack Yu <jackcwyu@google.com>
2025-03-03 11:33:28 +0100

    sm8350-common: Create feature flags for OMAPI readers
    Bug: 131062577
    Test: CTS OMAPI
    Change-Id: I69f3f913fdf3daf49ac74fede4e61282d1ff9d55

--- common.mk
+++ common.mk
@@ -27,6 +27,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.sd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.sd.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service \
