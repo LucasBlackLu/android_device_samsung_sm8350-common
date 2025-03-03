c17970ffd96f9a72ccd198003a805ec3e19b58f0
Andrzej Szafran <rtd1250@gmail.com>
2025-03-04 00:20:09 +0100

    sm6375-common: Include hardware/samsung FCM
    Change-Id: Icb1a1bd6de56cbf2419cb8a046fa2a7a35cfc912

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,7 +81,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    $(COMMON_PATH)/compatibility_matrix.device.xml
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/manifest_lahaina.xml
