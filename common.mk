0a1e4eb9ec6425e6681e90b8ca8ce017de9be1ee
Chih-Chung Chang <chihchung@google.com>
2025-03-03 11:12:36 +0100

    sm6375-common: Unhide new API for supporting multiple cameras
    Change-Id: I916a536c80330444a15986ba380205a6deed2390

--- common.mk
+++ common.mk
@@ -151,6 +151,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
 
 # Charger
 PRODUCT_PACKAGES += \
