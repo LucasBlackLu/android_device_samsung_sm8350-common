f55e1cb42c160b584d4cc6c6a99ae136b95348e8
Dan Morrill <morrildl@google.com>
2025-03-03 11:46:54 +0100

    sm6375-common: Declare support for a hardware gyroscope
    Change-Id: Ic0e57b36146b420c46af2c8b9770a1624b9ce3df

--- common.mk
+++ common.mk
@@ -522,6 +522,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
 
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
