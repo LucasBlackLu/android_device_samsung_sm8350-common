996e9d1f395b3cf581e015c7b1e5f94508bb87b5
Michael Bestas <mkbestas@lineageos.org>
2025-03-03 11:51:44 +0100

    sm6375-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- common.mk
+++ common.mk
@@ -554,6 +554,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung \
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.dual_role_usb \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
