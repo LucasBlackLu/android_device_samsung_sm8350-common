6f2672cf839994e505e1a6f18684f9414e26f73b
Marc Bourgoin <themard85@gmail.com>
2025-03-03 11:21:47 +0100

    sm6375-common: Build `vendor.qti.hardware.capabilityconfigstore` from source
    Change-Id: I9bd58ddae3c1b3868db23dc44dfb80ad337e0e1b

--- common.mk
+++ common.mk
@@ -191,6 +191,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
