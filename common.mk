abb0e042abd044160662b5c86ee6bf136b7c47d6
Patrick Lower <devvortex@gmail.com>
2025-03-03 11:16:30 +0100

    sm6375-common: Build the `SamsungDoze` from source
    * Built from hardware/samsung.
    
    Change-Id: Id0849fb5c17ff569d771a19cebfc579dc93dbc48

--- common.mk
+++ common.mk
@@ -363,6 +363,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDAP \
 
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze \
+
 # Seccomp Policy
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/seccomp_policy,$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy) \
