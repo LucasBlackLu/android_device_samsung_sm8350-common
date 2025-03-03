697ee585739b493bc08dcdad1f637d7cbb179c8f
LuK1337 <priv.luk@gmail.com>
2025-03-03 11:27:18 +0100

    sm6375-common: Build `libavservices_minijail_vendor` from source
    * Needed for vendor.qti.media.c2@1.0-service.
    
    Change-Id: I824468386dca2707e20b141f74ac65102ba6cf45

--- common.mk
+++ common.mk
@@ -295,6 +295,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
+    libavservices_minijail_vendor \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
