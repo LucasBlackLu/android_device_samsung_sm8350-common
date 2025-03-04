03848b94ddffba063bda6985f3cf596aab537716
Jan Altensen <info@stricted.net>
2025-03-04 14:16:00 +0100

    sm6375-common: Exclude audiofx
    Change-Id: Ic04fdeeae61056edfa9d17829ba9638195722b98

--- common.mk
+++ common.mk
@@ -111,6 +111,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Automotive/Gearhead
 PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@2.0.vendor \
