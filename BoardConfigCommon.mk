22c0ffa0c19fc2dec7c45e4a57aaf0452fbdbc2b
LucasBlackLu <lucasblack997@gmail.com>
2025-03-15 22:24:07 +0800

    sm8350-common: Move samsung manifest to device framewroks

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
