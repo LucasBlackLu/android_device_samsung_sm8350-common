f31232b7ab3b46f64b275a29ea96bf935565d3b1
LucasBlackLu <lucasblack997@gmail.com>
2025-03-29 23:55:32 +0800

    sm8350-common: Set selinux=permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,6 +62,7 @@ TARGET_LINUX_KERNEL_VERSION := 5.4
 
 # Kernel flags
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket printk.devkmsg=on pcie_ports=compat cpuinfo.chipname=SM8350 panic=4
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive 
 BOARD_BOOT_HEADER_VERSION := 3
 
 BOARD_KERNEL_BASE            := 0x00000000
