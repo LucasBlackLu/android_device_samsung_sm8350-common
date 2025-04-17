e939ff1da1777ac166907f96c79d86af5849b07f
Dan Morrill <morrildl@google.com>
2025-04-17 19:05:44 +0200

    sm6375-common: Declare standard set of features for a GSM phone
    Change-Id: I5b10c52f9744b437f07ce2e4ee4f841c4fd22540

--- common.mk
+++ common.mk
@@ -538,6 +538,7 @@ PRODUCT_BOOT_JARS += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
