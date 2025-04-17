b025b8b318aff99dff271f6470e91a858c3175b0
Haky86 <hakamelassouad@gmail.com>
2025-04-17 16:51:48 +0200

    sm6375-common: Setup default partitions flags
    Change-Id: I5464d79e4f20faabe3d9a8ee9d783573b2e11364

--- common.mk
+++ common.mk
@@ -61,5 +61,9 @@ PRODUCT_PACKAGES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
 
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm6375-common/sm6375-common-vendor.mk)
