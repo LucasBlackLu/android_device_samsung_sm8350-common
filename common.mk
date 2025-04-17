178d3f129cd423ea00064b8887b6f643c072073e
Kyle Harrison <khwebmail@gmail.com>
2025-04-17 19:04:22 +0200

    sm6375-common: Build telephony-ext from source
    Change-Id: Id545a6998ec17fe61fff53702001123971807ee3

--- common.mk
+++ common.mk
@@ -564,6 +564,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext \
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
