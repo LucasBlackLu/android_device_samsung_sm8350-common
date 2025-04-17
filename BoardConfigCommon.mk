2a4c383a7894452b33e535018de1f4f888cd7523
Haky86 <hakamelassouad@gmail.com>
2025-04-17 16:54:57 +0200

    sm6375-common: Setup default RIL flags
    Change-Id: Ie9eecc8794d554ded7bcb2f34b4e9b8697502aba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -207,5 +207,8 @@ TARGET_USERIMAGES_SPARSE_F2FS_DISABLED := false
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # Get non-open-source specific aspects
 include vendor/samsung/sm6375-common/BoardConfigVendor.mk
