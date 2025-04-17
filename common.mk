e208cf302b882cc0b36818c39ecdbca57617286f
Steve Pfetsch <spfetsch@google.com>
2025-04-17 19:16:47 +0200

    sm6375-common: Build the WPA/WPA2/IEEE 802.1X supplicant
     * wpa_supplicant is a WPA supplicant for Linux, BSD,
       Mac OS X, and Windows with support for WPA and WPA2
       IEEE 802.11i / RSN).
    
     * It is suitable for both desktop/laptop computers and
       embedded systems. Supplicant is the IEEE 802.1X/WPA
       component that is used in the client stations.
    
     * It implements key negotiation with a WPA Authenticator
       and it controls the roaming and IEEE 802.11 authentication
       & association of the WLAN driver.
    
    Change-Id: Ib91e2a8149062c160bcc74bf948879ed90efc131

--- common.mk
+++ common.mk
@@ -621,13 +621,14 @@ PRODUCT_PACKAGES += \
     hostapd \
     TetheringConfigOverlay \
     WifiOverlay \
+    wpa_supplicant \
+    wpa_supplicant.conf \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/icm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/icm.conf \
     $(COMMON_PATH)/configs/wifi/indoorchannel.info:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/indoorchannel.info \
     $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(COMMON_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 
 # Get non-open-source specific aspects
