95b1ef24e10010abe3daae8b19c70292bbfdfc5f
XiNGRZ <chenxingyu92@gmail.com>
2025-04-17 17:10:41 +0200

    sm6375-common: releasetools: Add dtbo image to the output zip
    * If present.
    
    Change-Id: I8bb4da99945eceae3beab77489e6b9e2a47ab972

---
+++ releasetools/releasetools.py
@@ -0,0 +1,25 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import common
+
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+
+def AddImage(info, basename, dest):
+  path = "IMAGES/" + basename
+  if path not in info.input_zip.namelist():
+    return
+
+  data = info.input_zip.read(path)
+  common.ZipWriteStr(info.output_zip, basename, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+
+def OTA_InstallEnd(info):
+  info.script.Print("Patching firmware images...")
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
