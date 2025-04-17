1ddf85557424359368a52400ba983eb7decd0516
Stricted <info@stricted.net>
2025-04-17 19:07:14 +0200

    sm6375-common: Build textclassifier bundle
    Change-Id: I326101539f689589e85691fe83e4076939f080e3

--- common.mk
+++ common.mk
@@ -541,5 +541,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.bundle1 \
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
