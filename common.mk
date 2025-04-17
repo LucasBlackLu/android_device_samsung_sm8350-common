8f9298858a93aadda4d26de9c5889de0129a7267
Tim Zimmermann <tim@linux4.de>
2025-04-17 18:58:28 +0200

    sm6375-common: Switch to AIDL sensors HAL
    * Built from hardware/samsung.
    
    Change-Id: I909510578145057be69041c71836e0a3b436f7b7

--- common.mk
+++ common.mk
@@ -544,7 +544,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors-service.samsung-multihal \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
