05cdef53a3a1a85fda59e9b3a44fce3fbc33ffce
Haky86 <hakamelassouad@gmail.com>
2025-04-17 17:40:57 +0200

    sm6375-common: Generate the basic props
    * Taken from a23xqnsxx-user 14 UP1A.231005.007 A236BXXS9EYA1 release-keys.
    
    Change-Id: I22e67d00bfc0ceaf865de42907aa8b8f043e8137

--- common.mk
+++ common.mk
@@ -107,9 +107,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service \
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.gatekeeper=mdfpp \
-
 # GNSS/GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0 \
@@ -149,10 +146,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung \
     libkeymaster4support.vendor:64 \
     libkeymaster4_1support.vendor:64 \
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.keystore=mdfpp \
-    ro.security.keystore.keytype=sakv2,gak \
 
 # Media
 PRODUCT_COPY_FILES += \
