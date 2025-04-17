1ee9fedd976f96739dc6c6c72243cd1cf84b23e5
Dan Morrill <morrildl@google.com>
2025-04-17 19:05:34 +0200

    sm6375-common: Declare that we have a telephony features
    Change-Id: I14abfba8cd78e54e4edda9c9d0b1640e146676f1

--- common.mk
+++ common.mk
@@ -577,6 +577,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext \
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
