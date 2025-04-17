6a49106c9be0e84f56f7266452bfd2410cd98885
Haky86 <hakamelassouad@gmail.com>
2025-04-17 18:57:45 +0200

    sm6375-common: Build the AOSP RIL libs from source
    * Fix Baseband and dual sim detection.
    
    Change-Id: I10a746914d17bb389a9e725056b2b173b18591f5

--- common.mk
+++ common.mk
@@ -512,8 +512,18 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5 \
-    android.hardware.radio.config@1.2 \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.config-V1-ndk.vendor:64 \
+    android.hardware.radio.data-V1-ndk.vendor:64 \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.radio.messaging-V1-ndk.vendor:64 \
+    android.hardware.radio.modem-V1-ndk.vendor:64 \
+    android.hardware.radio.network-V1-ndk.vendor:64 \
+    android.hardware.radio.sim-V1-ndk.vendor:64 \
+    android.hardware.radio.voice-V1-ndk.vendor:64 \
+    android.hardware.secure_element@1.0.vendor \
+    librilutils \
     libxml2 \
 
 # SamsungAdvancedDisplay
