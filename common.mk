e79c810a1941427ec8a40f8cf7dcdb97c9646dd6
Matthew Xie <mattx@google.com>
2025-04-17 17:53:05 +0200

    sm6375-common: Declare support for the Bluetooth Low Energy API
    * The feature is used to differ platforms that support Bluetooth low engery radio.
    
    Change-Id: I50844f40550fe17fb4ce2ee36c56b488cd30ba0a

--- common.mk
+++ common.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
 
 # Camera
 PRODUCT_PACKAGES += \
