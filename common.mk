216d823705015c7fa494e2593cf203eed05fcce3
Jan Altensen <info@stricted.net>
2025-04-17 19:07:50 +0200

    sm6375-common: Build the Samsung touch service from source
    * Built from hardware/samsung.
    
    Change-Id: I91e4918191781e697b0c6c6e5cfe5dbe72ea5e59

--- common.mk
+++ common.mk
@@ -582,6 +582,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
