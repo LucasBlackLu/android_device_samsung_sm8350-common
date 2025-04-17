f1d424bf40cf38432b1b924117684b81a941a853
Etan Cohen <etancohen@google.com>
2025-04-17 19:17:40 +0200

    sm6375-common: Declare support for WiFi Aware
    Rename only (and any reformatting needed to pass lint) - no
    functional changes!
    
    (cherry-pick of commit 49f8c84e1872c47716ebc2c04acdc8e3f6192734)
    (cherry-pick of commit 93852c97c636beaf6f9db0ab3994cac59cc83bf4)
    
    Bug: 32263750
    Test: All unit tests and integration (sl4a) tests pass.
    Change-Id: I00700206c90c9e6a1a0a2a79248dd571fe43ec26

--- common.mk
+++ common.mk
@@ -638,5 +638,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm6375-common/sm6375-common-vendor.mk)
