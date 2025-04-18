7957a036cf2491c9e66304edbd4caeca4e1d7980
Tim Zimmermann <tim@linux4.de>
2025-04-19 01:29:22 +0200

    sm6375-common: Switch to AIDL fingerprint HAL
    Change-Id: I53fe84f47c7289c012dfc58003040b85f80fd47b

--- common.mk
+++ common.mk
@@ -232,8 +232,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.face@1.0.vendor \
-    android.hardware.biometrics.fingerprint@2.1.vendor \
+    android.hardware.biometrics.fingerprint-service.samsung \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
