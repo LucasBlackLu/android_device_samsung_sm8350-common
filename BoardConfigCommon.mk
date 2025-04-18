5b824b740d83d273ff8aff16c14f09af215b74a5
Bruno Martins <bgcngm@gmail.com>
2025-04-18 14:45:20 +0200

    sm6375-common: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,7 +122,7 @@ DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/manifest.xml
 
 DEVICE_MATRIX_FILE := \
-    $(COMMON_PATH)/compatibility_matrix.xml
+    hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_sm6375
