21d99ed21556eb9abff6e3770f7ef3bad7041134
DY3413 <milosix3413@gmail.com>
2025-04-18 14:32:54 +0200

    sm6375-common: Build missing vndk libraries
    * These blobs are not built by default after 14 QPR3.
    
    Change-Id: Idbd8b11bda0c3563a0a178ecae114382b14d5f88
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- common.mk
+++ common.mk
@@ -50,6 +50,7 @@ PRODUCT_PACKAGES += \
     cplay \
     libaudiofoundation \
     libaudioroute \
+    libaudioroute.vendor \
     libaudioutils \
     libtinyalsa \
     libtinycompress \
@@ -511,6 +512,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.sim-V1-ndk.vendor:64 \
     android.hardware.radio.voice-V1-ndk.vendor:64 \
     android.hardware.secure_element@1.0.vendor \
+    libnetutils.vendor \
     librilutils \
     libxml2 \
 
