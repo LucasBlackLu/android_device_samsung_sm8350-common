d148cd8bee0e790579093b6cd70d737d7734094d
chandu078 <chandudyavanapelli03@gmail.com>
2025-04-20 03:11:18 +0200

    sm6375-common: Add libshim_ui dependency to unihal_android.so
    The symbol _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_
    and _ZN7android19GraphicBufferMapper6unlockEPK13native_handle was removed after 14QPR2 / 24Q1
    
    Change-Id: I702a4b4977987cbe16ad4a2dec2ab9fdf2b0e586
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -34,6 +34,7 @@ function blob_fixup() {
             ;;
         vendor/lib64/unihal_android.so)
              ${PATCHELF} --add-needed "libshim_sensorndkbridge.so" "${2}"
+             ${PATCHELF} --add-needed "libui_shim.so" "${2}"
              ;;
     esac
 }
