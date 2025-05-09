93ee0f1d1ad61bcf0fd0ab5890ade7988e5e80fd
LucasBlackLu <lucasblack997@gmail.com>
2025-05-09 20:28:15 +0800

    sm8350-common: Import the rootdir files

--- common.mk
+++ common.mk
@@ -36,9 +36,58 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    fstab.ramplus \
+    fingerprint_common.rc \
+    init.fingerprint.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qti.display_boot.rc \
+    init.qti.kernel.rc \
+    init.qti.media.rc \
+    init.ramplus.rc \
+    init.samsung.bsp.rc \
+    init.samsung.display.rc \
+    init.samsung.connector.rc \
+    init.samsung.rc \
+    init.target.rc \
+    init.vendor.rilcommon.rc \
+    init.vendor.sensors.rc \
+    init.vendor.sysfw.rc \
+    ueventd.qcom.rc \
+    vendor.qti.diag.rc \
+    vendor.samsung.rilchip.qcom.rc \
+    vendor.sensors.qti.rc \
+    vendor.sensors.sscrpcd.rc \
+    wifi_brcm.rc \
+    wifi_sm8350.rc \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/vendor/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+
+# Ramdisk Scripts
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.kernel.post_boot.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.chg_policy.sh \
+    init.qti.display_boot.sh \
+    init.qti.kernel.sh \
+    init.qti.media.sh \
+    init.qti.qcv.sh \
+    qca6234-service.sh \
+    vendor_modprobe.sh \
 
 # Recovery
 PRODUCT_COPY_FILES += \
