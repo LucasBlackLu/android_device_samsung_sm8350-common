f3efb5714d0deb3d1cdf6ffd1aecd86650da0795
LucasBlackLu <lucasblack997@gmail.com>
2025-05-09 20:28:15 +0800

    sm8350-common: Initial the rootdir

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -22,3 +22,20 @@ LOCAL_MODULE_STEM  := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_SRC_FILES    := vendor/etc/fstab.qcom
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.ramplus
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+LOCAL_MODULE_TAGS  := optional
+LOCAL_SRC_FILES    := vendor/etc/fstab.ramplus
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_SRC_FILES    := vendor/ueventd.qcom.rc
+include $(BUILD_PREBUILT)
