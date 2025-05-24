f671d8f83b397dd20f9c23004281c610a219e490
LucasBlackLu <lucasblack997@gmail.com>
2025-05-24 19:36:23 +0800

    Revert "sm6375-common: Build bluetooth stack from source"
    This reverts commit 83480a8880d23808fb8480d5f09246a717f67a11.

--- common.mk
+++ common.mk
@@ -87,13 +87,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth@1.0.vendor \
     aptxalsOverlay \
-    audio.bluetooth.default \
-    libbluetooth_audio_session \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
