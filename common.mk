7aca1a9e20a172da7c69a9c3ebd885a4f7c89a59
LucasBlackLu <lucasblack997@gmail.com>
2025-05-25 17:15:01 +0800

    Revert "sm6375-common: overlay: Add Qualcomm® aptX™ audio codec overlays"
    This reverts commit 451a4286411ba012fb599a1943182489b283e63f.

--- common.mk
+++ common.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
-    aptxalsOverlay \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
