4ba427b4639da088e16e051391ab0cfc4fec80d8
LucasBlackLu <lucasblack997@gmail.com>
2025-05-28 19:23:25 +0800

    sm8350-common: Regenerate ril

--- common.mk
+++ common.mk
@@ -453,18 +453,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.config-V1-ndk.vendor:64 \
-    android.hardware.radio.data-V1-ndk.vendor:64 \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.radio.messaging-V1-ndk.vendor:64 \
-    android.hardware.radio.modem-V1-ndk.vendor:64 \
-    android.hardware.radio.network-V1-ndk.vendor:64 \
-    android.hardware.radio.sim-V1-ndk.vendor:64 \
-    android.hardware.radio.voice-V1-ndk.vendor:64 \
-    android.hardware.secure_element@1.0.vendor \
+    libjsoncpp.vendor \
     libnetutils.vendor \
     librilutils \
+    librmnetctl \
     libxml2 \
+    secril_config_svc \
 
 # SamsungAdvancedDisplay
 PRODUCT_PACKAGES += \
