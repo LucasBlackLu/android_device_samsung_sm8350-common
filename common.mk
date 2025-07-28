3349077ced40134873409615d0c573aa643632b8
LucasBlackLu <lucasblack997@gmail.com>
2025-07-28 21:42:49 +0800

    sm8350-common: Build media HAL

--- common.mk
+++ common.mk
@@ -50,6 +50,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore=mdfpp \
     ro.security.keystore.keytype=sakv2,gak \
 
+# Media
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_vndk.vendor \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
+    $(LOCAL_PATH)/media/media_codecs_performance_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_lahaina.xml \
+
+# Minijail
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
