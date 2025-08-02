cad8b500fd9ac16c4885d5e583cbb0fa1bd492fe
LucasBlackLu <lucasblack997@gmail.com>
2025-08-03 01:04:33 +0800

    sm8350-common: Add kernel flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,6 +39,33 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 TARGET_BOOTLOADER_BOARD_NAME := lahaina
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := \
+    androidboot.hardware=qcom androidboot.memcg=1 \
+    androidboot.selinux=permissive \
+    androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 \
+    cgroup.memory=nokmem,nosocket \
+    cpuinfo.chipname=SM8350 \
+    console=null \
+    firmware_class.path=/vendor/firmware_mnt/image \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    panic=4 \
+    pcie_ports=compat \
+    printk.devkmsg=on \
+    service_locator.enable=1
+
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS :=  --header_version $(BOARD_BOOT_HEADER_VERSION)
+TARGET_KERNEL_SOURCE := kernel/samsung/sm8350
+TARGET_MODULE_ALIASES += wlan.ko:qca_cld3_wlan.ko
+
 # Platform
 PRODUCT_PLATFORM := lahaina 
 TARGET_BOARD_PLATFORM := lahaina
