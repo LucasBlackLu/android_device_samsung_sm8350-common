293d5ee874616d72ce399dc353fee65098cbc7e7
Dianne Hackborn <hackbod@google.com>
2025-09-22 01:21:38 +0200

    sm8350-common: Add location related features for GPS enabled targets
    Change-Id: Ibecf665cb21b89fef09951ece379f9f53353698f

--- common.mk
+++ common.mk
@@ -180,6 +180,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# GPS
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
