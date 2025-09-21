cea501b06ba71b20d453e6ada9bf99a8c78af821
Bruno Martins <bgcngm@gmail.com>
2025-09-22 00:20:44 +0200

    sm6375-common: Opt-in for Samsung-specific RFS symlinks
    * Not that it matters in this case, but aligns with stock and all Samsung devices based on Qualcomm.
    
    Change-Id: Ica8fa5567bcb2999ec46f27ba372a7d7794e8783

--- common.mk
+++ common.mk
@@ -10,6 +10,7 @@ PRODUCT_ENABLE_UFFD_GC := false
 
 # Partitions
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
+$(call soong_config_set,rfs,mpss_firmware_symlink_target,firmware_modem)
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
