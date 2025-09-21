273226b43d1f00e00aef55c57154e0ccc0c2148e
Adam Shih <adamshih@google.com>
2025-09-22 00:16:27 +0200

    sm8350-common: Create a makefile that holds non AB device setting
    Bug: 189894781
    Test: build ROM with the binary inside
    Change-Id: I99f2b4d3bd57b57ffd3b5066b1306c6fa17c2c85

--- common.mk
+++ common.mk
@@ -90,5 +90,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # Storages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Updater
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
