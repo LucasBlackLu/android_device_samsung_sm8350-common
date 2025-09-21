0eb56ea382abc1aa0f4afe8d9b4942ff12017044
Mike Lockwood <lockwood@android.com>
2025-09-22 01:45:58 +0200

    sm6375-common: Add platform features for USB host and USB accessory support
    Change-Id: I167de7c7e20a51201d7164ecba285da17d08ceda

--- common.mk
+++ common.mk
@@ -434,6 +434,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
     android.hardware.usb-service.samsung
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
