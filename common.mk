c107490b359b16d6f6edfcdf09404c9af31ff1a3
LucasBlackLu <lucasblack997@gmail.com>
2025-09-22 00:27:32 +0200

    sm8350-common: Import init files from stock
    Change-Id: I53fd28a36eca3fa98a9bddba37835d007b73c5d5

--- common.mk
+++ common.mk
@@ -7,7 +7,39 @@ COMMON_PATH := device/samsung/sm8350-common
 
 # Init
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fingerprint_common.rc \
+    fstab.qcom \
+    fstab.ramplus \
+    init.audio.samsung.rc \
+    init.fingerprint.rc \
+    init.nfc.samsung.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qti.display_boot.rc \
+    init.recovery.qcom.rc \
+    init.samsung.bsp.rc \
+    init.samsung.display.rc \
+    init.samsung.connector.rc \
+    init.samsung.rc \
+    init.target.rc \
+    init.vendor.rilcommon.rc \
+    init.vendor.sensors.rc \
+    init.vendor.sysfw.rc \
+    ueventd.qcom.rc \
+    vendor.samsung.rilchip.qcom.rc
+
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.display_boot.sh \
+    qca6234-service.sh
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/init/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
