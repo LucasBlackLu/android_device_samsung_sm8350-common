19de889ff3ab9334c9cc1664fdcadca7c0d1fba8
Haky86 <hakamelassouad@gmail.com>
2025-09-22 00:12:28 +0200

    sm6375-common: Setup default recovery flags
    * Build and boot a working LineageOS recovery.
    
    Change-Id: Icc5e7fddce0765a73afc0ce4c108e102824a28fa

--- common.mk
+++ common.mk
@@ -9,6 +9,14 @@ COMMON_PATH := device/samsung/sm8350-common
 $(call soong_config_set,rfs,mpss_firmware_symlink_target,firmware_modem)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/init/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
+    $(COMMON_PATH)/init/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
@@ -17,6 +25,10 @@ PRODUCT_PACKAGES += \
     vendor_dsp_mountpoint \
     vendor_firmware_mnt_mountpoint \
     vendor_firmware-modem_mountpoint
+
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.qcom.rc
 
 # Shipping API
 BOARD_SHIPPING_API_LEVEL := 30
