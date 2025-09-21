2660b19562336d977e724f3e7e28b150c38bd6f5
chaptsand <chaptsand@gmail.com>
2025-09-22 01:00:44 +0200

    sm8350-common: Import audio@7.1 default impl from android-14.0.0_r29
    Change-Id: I2c71b180a1632dbf1fa164f6c4f7e35a6d8dff27

--- common.mk
+++ common.mk
@@ -25,7 +25,7 @@ PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@6.0-impl.samsung-sm8350 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
