89b6eee9be497c91a32c3d5497ec68badc6ab93c
Dan Morrill <morrildl@google.com>
2025-09-22 01:05:34 +0200

    sm8350-common: Declare support for the Bluetooth API
    Change-Id: I1ab85b3085af8c2ff3bbf6db368439075101849f

--- common.mk
+++ common.mk
@@ -108,6 +108,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
