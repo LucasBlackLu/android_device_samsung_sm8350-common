4b7e9057adb5c771abd5378b7909eed1359478b5
LuK1337 <priv.luk@gmail.com>
2025-09-22 00:21:04 +0200

    sm8350-common: Create vendor mount points
    Change-Id: I13dc0d47145d1ca6dc1fdceb091fcc7c83cb8637

--- common.mk
+++ common.mk
@@ -8,6 +8,11 @@ COMMON_PATH := device/samsung/sm8350-common
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+
 # Shipping API
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
