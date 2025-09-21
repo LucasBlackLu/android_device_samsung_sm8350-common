53b82a04ad7f73e38e8f1a94b481b646c6708eea
Michael Bestas <mkbestas@lineageos.org>
2025-09-22 01:44:42 +0200

    sm6375-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- common.mk
+++ common.mk
@@ -398,6 +398,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 AB_OTA_UPDATER := false
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.dual_role_usb \
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
