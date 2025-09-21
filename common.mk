125bc3fa4c73f655bf3cc4b8fc6371ab154dec57
Adam Connors <adamconnors@google.com>
2025-09-22 01:30:29 +0200

    sm6375-common: Add feature flags for verified boot and securely remove users
    Bug: 17405636
    Change-Id: I06c0319f7903787910c52d65025b0d77d9e0575f

--- common.mk
+++ common.mk
@@ -271,6 +271,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
 
 # Power
 PRODUCT_COPY_FILES += \
