eaba5bc0175894e885a86b5a5b0c64ba3829d276
Eric Laurent <elaurent@google.com>
2025-09-22 00:22:50 +0200

    sm8350-common: Add Bluetooth Hearing Aid routing and configuration
    Bug: 69623109
    Test: connect to Hearing Aid, make phone call, verify audio is heard
    Change-Id: I92267fb48608fb721c5c4ef7ec9fceeafc8ea192

--- common.mk
+++ common.mk
@@ -32,6 +32,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/audio/configs/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
