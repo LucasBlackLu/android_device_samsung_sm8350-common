6f6409dfe2fdfbac9c0d6b7476582870201d518a
Pooja Kumari <kumarip@codeaurora.org>
2025-09-22 01:22:32 +0200

    sm8350-common: Build IPACM (IP Catalogue and IP Management)
    Change-Id: I6b73948ed0658f512688fe6bf66a1b029f40390a

--- common.mk
+++ common.mk
@@ -159,6 +159,11 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/init/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(COMMON_PATH)/init/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
