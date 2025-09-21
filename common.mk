350278fb8c52f192496d93015589ca7bafd7ed5f
LuK1337 <priv.luk@gmail.com>
2025-09-22 01:22:49 +0200

    sm8350-common: Build keymaster HIDL service from source
    Change-Id: I2a5f9c33baadae5389e6199d9cef0b3411fe57f7

--- common.mk
+++ common.mk
@@ -171,6 +171,9 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 # Keymaster
 $(call soong_config_set,samsungVars,target_keymaster4_library,//vendor/samsung/sm8350-common:libskeymaster4device)
 
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-service.samsung
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
