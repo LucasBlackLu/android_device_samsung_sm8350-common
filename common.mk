220cdbebce75449a20a04330783cc171e8d97a55
LuK1337 <priv.luk@gmail.com>
2025-09-22 01:08:38 +0200

    sm8350-common: Migrate to soong_config_set()
    Change-Id: I06de8759836114f54a822d6f23f1177515e21178

--- common.mk
+++ common.mk
@@ -112,6 +112,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Camera
+$(call soong_config_set,samsungCameraVars,extra_ids,54)
+$(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
