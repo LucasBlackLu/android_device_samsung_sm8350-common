0e81a7bc478089414960475751968c7a8eb8e003
Glenn Kasten <gkasten@google.com>
2025-09-22 01:04:02 +0200

    sm6375-common: Add XML for pro audio feature
    Bug: 23604253
    Change-Id: I480cf0a46c286badc6fd95a3b42e6cb1ed056b1b

--- common.mk
+++ common.mk
@@ -76,6 +76,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
 
 # Component Overrides
 PRODUCT_COPY_FILES += \
