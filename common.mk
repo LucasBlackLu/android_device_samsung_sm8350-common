ddebeae72561b0b866bbdbfc9a63d86f387fc6c0
Rashed Abdel-Tawab <rashed@linux.com>
2025-09-22 01:42:42 +0200

    sm6375-common: Build system telephony extension
    Change-Id: If3aaf88a908b2e6f6a5c873820981a40390197f6

--- common.mk
+++ common.mk
@@ -393,6 +393,10 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
