c24160c21c6b1301cc6e5c1c34c114ed1afb7293
Tim Zimmermann <tim@linux4.de>
2025-09-22 01:46:31 +0200

    sm8350-common: Build new AIDL vibrator HAL from source
    Change-Id: If10972cce06dd5128729d4d7409c57f79775b09b

--- common.mk
+++ common.mk
@@ -274,5 +274,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.samsung
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/sm8350-common/sm8350-common-vendor.mk)
