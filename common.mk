186f25d43fc05697110fe5e4e7eada4f0fb133ac
Bruno Martins <bgcngm@gmail.com>
2025-09-22 01:43:48 +0200

    sm6375-common: Build Touch AIDL from source
    Change-Id: Id4fcd7ca97b04f887dec6939419815722097d10c

--- common.mk
+++ common.mk
@@ -416,6 +416,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch-service.samsung
+
 # Updater
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
