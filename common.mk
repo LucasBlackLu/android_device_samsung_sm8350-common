64f0cce07a78cf10af938f7d68a38d77f46b3ba3
Hung-ying Tyan <tyanh@google.com>
2025-09-22 01:04:44 +0200

    sm6375-common: Add software features for SIP and VOIP
    * and block SipService creation and SIP API if the feature is not available.
    
    Change-Id: Icf780af1ac20dda4d8180cea3e5b20e21a8350bc

--- common.mk
+++ common.mk
@@ -78,6 +78,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Component Overrides
 PRODUCT_COPY_FILES += \
