b8205e7d57cce81d7959d29c95a1769e76b68864
Kyle Zhang <kelzhan@google.com>
2025-09-22 01:13:19 +0200

    sm8350-common: Use aidl drm interface
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I01ceb9bdb60ff6d9323acee3d7140b68b31a465f

--- common.mk
+++ common.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # Graphics
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
