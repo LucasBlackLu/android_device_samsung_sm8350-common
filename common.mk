68534489cc46518f34dfa370abece57234673c65
Kyle Zhang <kelzhan@google.com>
2025-09-22 01:13:19 +0200

    sm8350-common: Use aidl drm interface
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I01ceb9bdb60ff6d9323acee3d7140b68b31a465f

--- common.mk
+++ common.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1.vendor:64
