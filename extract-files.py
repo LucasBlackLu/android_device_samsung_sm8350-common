5b7cc40a1bf6ea0f461f858d02875a08a040bda8
LuK1337 <priv.luk@gmail.com>
2025-09-22 01:24:01 +0200

    sm6375-common: Shim Samsung keymaster HAL impl with libcrypto-v33.so
    * Current asn1t.h structs aren't binary compatible with our keymaster.
    
    Change-Id: Id78b71c1f08216f861a033097cd015ba3c109925

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,8 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/libsec-ril.so': blob_fixup()
         .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
         .sig_replace('E1 03 15 AA 08 00 40 F9 E3 03 14 AA 08 09 40 F9', 'E1 03 15 AA 08 00 40 F9 03 00 80 D2 08 09 40 F9'),
+     ('vendor/lib64/libkeymaster_helper.so', 'vendor/lib64/libskeymaster4device.so'): blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
      ('vendor/lib/libwvhidl.so', 'vendor/lib/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
