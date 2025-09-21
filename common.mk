4a486bd7bb51c364712e593c2acdb17ee8c3fcd4
Jan Altensen <info@stricted.net>
2025-09-22 01:05:03 +0200

    sm8350-common: Exclude audiofx
    Change-Id: Ic04fdeeae61056edfa9d17829ba9638195722b98

--- common.mk
+++ common.mk
@@ -100,6 +100,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
