0a7de3cb0364582478443daab4036cabe761a3fa
Tim Zimmermann <tim@linux4.de>
2025-09-22 01:45:34 +0200

    sm6375-common: Build Samsung's usb gadget HAL from source
    Change-Id: Ia9a801a38928348348e9d6b2853080a00983ccb0

--- common.mk
+++ common.mk
@@ -432,6 +432,7 @@ AB_OTA_UPDATER := false
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb-service.samsung
 
 # WiFi
 PRODUCT_COPY_FILES += \
