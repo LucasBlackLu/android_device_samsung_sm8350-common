af0f2fb34c0dd943be0544c31df65356c612896a
Tim Zimmermann <tim@linux4.de>
2025-09-22 01:38:45 +0200

    sm8350-common: Build AIDL sensors HAL from source
    Change-Id: I909510578145057be69041c71836e0a3b436f7b7

--- common.mk
+++ common.mk
@@ -355,6 +355,9 @@ PRODUCT_PACKAGES += \
     SamsungDAP
 
 # Sensor
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-service.samsung-multihal \
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
