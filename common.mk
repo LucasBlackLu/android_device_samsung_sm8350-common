1130acbc095067112390eb40a12b5ff9dc992901
Alexander Koskovich <zvnexus@outlook.com>
2025-09-22 01:13:01 +0200

    sm8350-common: Disable the usage of ConfigStore
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- common.mk
+++ common.mk
@@ -57,6 +57,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_29.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_29.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
