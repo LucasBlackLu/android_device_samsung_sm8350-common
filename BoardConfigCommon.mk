cb2d43eb9bd22e76f5543f184d133791185b6145
jmpfbmx <jmpf.bmx@gmail.com>
2025-09-22 00:04:01 +0200

    sm8350-common: Add arch, bootloader and platform flags
    Change-Id: Ie0ca1d06a57a715867ceb4954cc37248cd099081
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,10 +8,24 @@ COMMON_PATH := device/samsung/sm8350-common
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := kryo385
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
 # Bootanimation
 TARGET_BOOTANIMATION_HALF_RES := true
 
 # Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := lahaina
+TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 TARGET_NO_RPC := true
 
@@ -52,6 +66,10 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 
 # Media
 TARGET_USES_ION := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := lahaina
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
