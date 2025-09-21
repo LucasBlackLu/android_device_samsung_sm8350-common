5038644f394bf2ad4487a21803524fedf0b773e7
Steve Pfetsch <spfetsch@google.com>
2025-09-22 01:48:55 +0200

    sm6375-common: Build the WPA/WPA2/IEEE 802.1X supplicant
     * wpa_supplicant is a WPA supplicant for Linux, BSD,
       Mac OS X, and Windows with support for WPA and WPA2
       IEEE 802.11i / RSN).
    
     * It is suitable for both desktop/laptop computers and
       embedded systems. Supplicant is the IEEE 802.1X/WPA
       component that is used in the client stations.
    
     * It implements key negotiation with a WPA Authenticator
       and it controls the roaming and IEEE 802.11 authentication
       & association of the WLAN driver.
    
    Change-Id: Ib91e2a8149062c160bcc74bf948879ed90efc131

--- common.mk
+++ common.mk
@@ -450,6 +450,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
