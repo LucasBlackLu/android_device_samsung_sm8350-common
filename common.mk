6c31e672d20382171d632f3f3c4ab5bd18dd58b2
Bruno Martins <bgcngm@gmail.com>
2025-09-22 00:21:28 +0200

    sm6375-common: Add firmware-modem mount point
    * Found in Samsung Qualcomm-based devices.
    
    Change-Id: Ia81f87fa820cd43549ea9a88c8179ba0c2995316

--- common.mk
+++ common.mk
@@ -42,6 +42,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     vendor_dsp_mountpoint \
     vendor_firmware_mnt_mountpoint \
+    vendor_firmware-modem_mountpoint
 
 # Ramdisk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
