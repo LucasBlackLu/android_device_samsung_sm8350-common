b2a5c8e54bbb8558e3bdbef79a23662543d2e603
Tim Zimmermann <tim@linux4.de>
2025-09-22 01:38:19 +0200

    sm8350-common: Build secril_config_svc from source
    * This removes the need for the hack we've been using to
      get single SIM to work, this device doesn't have
      /mnt/vendor/efs/factory.prop leaving
      ro.vendor.multisim.simslotcount unset while it's being
      expected to be 1 in order to stop the second rild.
    
    Change-Id: Ife2b16a2df2549ed298d3c2f866861379428366e

--- common.mk
+++ common.mk
@@ -328,6 +328,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
+# RIL
+PRODUCT_PACKAGES += \
+    secril_config_svc \
+
 # Samsung DAP
 PRODUCT_PACKAGES += \
     SamsungDAP
