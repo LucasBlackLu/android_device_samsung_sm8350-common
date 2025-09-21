afc43541f8d903dc267af1b62680ce2eba7a20d2
Weilun Du <wdu@google.com>
2025-09-22 01:49:51 +0200

    sm6375-common: Add TetheringConfigOverlay
    BUG: 163800715
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- common.mk
+++ common.mk
@@ -455,6 +455,7 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
+    TetheringConfigOverlay \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
