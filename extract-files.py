96821d74a01cafc1d5521043b415623d386a258a
LuK1337 <priv.luk@gmail.com>
2025-09-22 01:02:01 +0200

    sm8350-common: audio-impl: Pass call state to audio HAL
    * This fixes incall audio in LTE variants.
    
    Change-Id: I9fdffcf97318b64429a952bda411c282c816a336

--- extract-files.py
+++ extract-files.py
@@ -48,6 +48,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
      'vendor/etc/vintf/manifest/sec_c2_manifest_default0.xml': blob_fixup()
         .regex_replace('default0', 'software'),
+     'vendor/lib64/libsec-ril.so': blob_fixup()
+        .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
