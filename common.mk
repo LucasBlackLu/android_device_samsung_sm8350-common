1684f8f29a05e9650a73e3933c212f9b1ac9b574
Kyle Zhang <kelzhan@google.com>
2025-09-22 01:13:19 +0200

    sm6375-common: Use aidl drm interface
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I01ceb9bdb60ff6d9323acee3d7140b68b31a465f

--- common.mk
+++ common.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # Fastboot
 TARGET_BOARD_FASTBOOT_INFO_FILE := $(COMMON_PATH)/fastboot-info.txt
 
