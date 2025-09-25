86a3b2a3733f766203444d331af6e29c93365ef2
LucasBlackLu <lucasblack997@gmail.com>
2025-09-25 14:58:13 +0800

    sm8350-common: Build thermal

--- common.mk
+++ common.mk
@@ -417,6 +417,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor \
+    android.frameworks.cameraservice.service@2.1.vendor
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.samsung
