18a7e85111b0237509f4049e72022819fd8e8a0a
LucasBlackLu <lucasblack997@gmail.com>
2025-09-28 00:51:57 +0800

    Revert "sm6375-common: Switch to AIDL fingerprint HAL"
    This reverts commit 3debad7dff968fe34992e11280e16e936836a84a.

--- common.mk
+++ common.mk
@@ -186,9 +186,6 @@ PRODUCT_PACKAGES += \
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.samsung
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
