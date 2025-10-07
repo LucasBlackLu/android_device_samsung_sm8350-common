833988556fd3493dc645f8c05b2eff626778b421
LucasBlackLu <lucasblack997@gmail.com>
2025-10-08 05:43:35 +0800

    sm8350-common: Build power 1.3

--- common.mk
+++ common.mk
@@ -239,6 +239,10 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint \
     vendor_firmware-modem_mountpoint
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3.vendor
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
