a7816bc85fa92dbae9aabf542cbe2da18c5dd55c
LucasBlackLu <lucasblack997@gmail.com>
2025-10-10 04:52:35 +0800

    sm8350-common: Build perf

--- common.mk
+++ common.mk
@@ -183,6 +183,13 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint \
     vendor_firmware-modem_mountpoint
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0.vendor:64
