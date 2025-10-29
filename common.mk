08fb34a6e57ec06a6839572915349bfbd3e6e524
LucasBlackLu <lucasblack997@gmail.com>
2025-10-29 14:50:58 +0800

    sm8350-common: Build android.hidl.allocator@1.0.vendor
    * Needed by vendor.qti.memory.pasrmanager

--- common.mk
+++ common.mk
@@ -211,6 +211,7 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     libhidltransport \
