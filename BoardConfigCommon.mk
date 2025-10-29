d2905fb78ae13f42ff1f1aa430c614c9b53aa36c
LucasBlackLu <lucasblack997@gmail.com>
2025-10-29 14:54:58 +0800

    sm8350-common: FCM: Add vendor.qti.memory.pasrmanager@1.1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,6 +63,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
