0d72d979af201e0e48be3a8e5cf5b84466cd75b3
LucasBlackLu <lucasblack997@gmail.com>
2025-11-03 16:50:06 +0800

    sm8350-common: Build android.hardware.gnss@2.1.vendor

--- common.mk
+++ common.mk
@@ -185,6 +185,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1.vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
