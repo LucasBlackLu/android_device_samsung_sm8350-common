270ab4a6985e2cb3719e758c0baace5bbfd7e77f
LucasBlackLu <lucasblack997@gmail.com>
2025-11-05 19:24:01 +0800

    Revert "sm8350-common: Build perf"
    This reverts commit d578f4fdd866b4fe536ba1170779d0d5a8fc61f2.

--- common.mk
+++ common.mk
@@ -340,13 +340,6 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint \
     vendor_firmware-modem_mountpoint
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
