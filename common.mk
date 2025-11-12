07624b0307ef2b874cee71fbc2d512543ff8e045
LucasBlackLu <lucasblack997@gmail.com>
2025-11-13 00:35:14 +0800

    sm8350-common: Build Camera HAL

--- common.mk
+++ common.mk
@@ -120,6 +120,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Camera
+PRODUCT_PACKAGES += \
+    libgrallocusage
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
